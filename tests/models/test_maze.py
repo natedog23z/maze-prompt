68fa1fe834c4bee9d601e311e3a8de6b651829d7
natedog23z <nathan@unitedpursuit.com>
2025-06-10 12:07:40 -0600

    test(models): add pytest round-trip for PromptTokens

--- tests/models/test_maze.py
+++ tests/models/test_maze.py
@@ -1,6 +1,12 @@
+import pytest
 from packages.models.maze import PromptTokens, EXAMPLE_TOKENS
 
 
 def test_load_and_sentence():
     tok = PromptTokens(**EXAMPLE_TOKENS)
-    assert tok.to_sentence() == "cat jumps rooftop" 
+    assert tok.to_sentence() == "cat jumps rooftop"
+
+
+def test_extra_fields_fail():
+    with pytest.raises(ValueError):
+        PromptTokens(**EXAMPLE_TOKENS, extra="oops") 
