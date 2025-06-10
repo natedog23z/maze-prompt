9570fb8c857655c31b761917fbc5dee01ff1821c
natedog23z <nathan@unitedpursuit.com>
2025-06-10 12:58:40 -0600

    test(models): add unit tests for Python PromptTokens model

--- tests/models/test_maze.py
+++ tests/models/test_maze.py
@@ -1,12 +1,10 @@
-import pytest
 from packages.models.maze import PromptTokens, EXAMPLE_TOKENS
+import pytest
 
-
-def test_load_and_sentence():
+def test_sentence_round_trip():
     tok = PromptTokens(**EXAMPLE_TOKENS)
     assert tok.to_sentence() == "cat jumps rooftop"
 
-
-def test_extra_fields_fail():
+def test_extra_keys_rejected():
     with pytest.raises(ValueError):
         PromptTokens(**EXAMPLE_TOKENS, extra="oops") 
